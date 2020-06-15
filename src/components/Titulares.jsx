eb2394e9191231bcfc0b3ffe85264b2008ffad49
cristobalobos <cristobal.lobost@gmail.com>
2020-06-14 20:45:48 -0400

    Titulares Component

---
+++ src/components/Titulares.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Titulares = ({ titulares }) => {
+    <section>
+        <div className="cancha">
+            {
+                titulares.map(j => (
+                    <article className="titular">
+                        <div>
+                            <img src={j.foto} alt={j.nombre}></img>
+                            <button>X</button>
+                        </div>
+                        <p>{j.nombre}</p>
+                    </article>
+                ))
+            }
+        </div>
+    </section>
+}
+
+const mapStateToProps = state => ({
+    titulares: state.titulares
+})
+
+
+const mapDispatchToProps = dispatch => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Titulares)
